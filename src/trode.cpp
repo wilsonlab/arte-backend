5ec5fd0f3601417631d777785408d737260fd2cd
Greg Hale <imalsogreg@gmail.com>
2010-12-19 21:35:14 +0000

    added skeletal trode.cpp file

---
+++ src/trode.cpp
@@ -0,0 +1,17 @@
+// object represents 1 tetrode.  Takes raw data in full buffers, filters the needed channels,
+// stores the filtered data, scans it for spikes, sends the spikes to a drawing object, and to
+// central file-saving object
+
+#include "trode.h"
+
+Trode::Trode(){
+}
+
+Trode::Trode(char *tn, ArteOpt *opt){
+
+}
+
+Trode::~Trode(){
+
+}
+
