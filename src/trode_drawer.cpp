fa61dc3c32b63447c2806928127f1ebfb35a718f
Greg Hale <imalsogreg@gmail.com>
2010-12-21 05:24:51 +0000

    trode_drawer back and forth..

--- src/trode_drawer.cpp
+++ src/trode_drawer.cpp
@@ -10,7 +10,8 @@
 // The trode can also keep 'saved', 'transmitted' flags for each spike,
 // and clear all those that have been drawn, saved, trans, whatever else.
 
-// a trode_drawer object then might inherit from Gtk::Window 
+// a trode_drawer object then might inherit from Gtk::Window?  Gtk::DrawingArea?
+// (in latter case, there would be a parent TrodeWindow inherriting from Gtk::Window) 
 // (if these can be made opengl-able?).  Instantiated during 
 // program init.  It will have an array of trode object pointers.
 // It will also hold the drawing preferences (zoom, color, accumulate traces, etc)
--- src/trode_drawer.h
+++ src/trode_drawer.h
@@ -1,22 +1,14 @@
 // trode_drawer class
-// should this and eeg_drawer both inherit from a sig_drawer class?
-// every trode object will have a single trode_drawer class
-// keeping track of
-// From input:
-//  * a list of the most recent spike waveforms
+// a type of Gtk::DrawingArea with a list of trodes, a list of drawing-areas, and the mapping
+// Keeps track of
+// 
 //  * a list of peaks (or other waveform features?) for all past spikes 
 //    (possibly just bitmap renderings of projections, w/ a white dot added at each new spike)
 // From config:
 // * a list of the drawing areas (openGL or matlab) to draw to
-// * a list of which channels to render waveforms for (probably all)
-// * a list of which projections to draw
-// * other preferences (line color, overlay mode, etc)
-//
-// Because the caller might not yet have a handle to the drawing area when
-// this object is constructed (during reading of the conf files), we'll probably
-// offload that part of initialization from the constructor to a fn called
-// after reading the conf file is finished (when we'll have a full count of trodes
-// and drawing areas needed)
+// * for each trode, a list of which channels to render waveforms for (probably all)
+// * for each trode, a list of which projections to draw
+// * for each trode, other preferences (line color, overlay mode, etc.)
 
 #include <iostream>
 #include "opts.h"
@@ -27,7 +19,7 @@ class TrodeDrawer{
 
  public:
   TrodeDrawer();
-  ~TrodeDrawer();
+  virtual ~TrodeDrawer();
 
   void remove_all_drawingareas();
   void attach_drawingarea();
