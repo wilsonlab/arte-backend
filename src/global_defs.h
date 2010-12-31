ea09b2a552f48ed9097bbbfd8560fa90ec2d54d6
Greg Hale <imalsogreg@gmail.com>
2010-12-31 20:08:14 +0000

    little change to global_defs.h

--- src/global_defs.h
+++ src/global_defs.h
@@ -1,5 +1,5 @@
 #include <string>
 
 const int MAX_TRODE_NAME_LENGTH = 50;
-const string default_setup_config_filename ("~/.arte-ephys/conf/arte_setup_default.conf");
-const string default_session_config_filename ("~/.arte-ephys/conf/arte_session_default.conf");
+const string default_setup_config_filename ("/home/greghale/arte-ephys/conf/arte_setup_default.conf");
+const string default_session_config_filename ("/home/greghale/arte-ephys/conf/arte_session_default.conf");
