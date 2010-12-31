a984c9d13a2d8ffd4bd05dd473c58be235bfc411
Greg Hale <imalsogreg@gmail.com>
2010-12-31 23:03:17 +0000

    minor

--- src/arteopt.cpp
+++ src/arteopt.cpp
@@ -5,28 +5,30 @@
 #include <boost/property_tree/exceptions.hpp>
 #include <assert.h>
 #include "global_defs.h"
+#include <iostream>
 
 using namespace boost::property_tree;
 
-void arte_init(int argc, char *argv[], std::string &setup_fn, std::string &session_fn){
+void arte_init(int argc, char *argv[], const std::string &setup_fn, const std::string &session_fn){
 
   if(!setup_fn.empty())
     setup_config_filename = setup_fn.data();
-  //setup_config_filename.str(setup_fn.str()) // copy contents of setup_fn argument into setup_config_filename
   else
-    setup_config_filename = default_setup_config_filename.data();
-  // setup_config_filename.str(default_setup_config_filename.str());  
+    setup_config_filename = default_setup_config_filename.data();  
   if(!session_fn.empty())
     session_config_filename = session_fn;    
-  //session_config_filename.str(session.fn.str());
   else
     session_config_filename = session_fn;
-  //    session_config_filename.str(default_session_config_filename.str());
+
+  std::string t2 = "test.conf";
+  read_xml(t2, setup_pt);
 
   try{
-    read_xml(setup_fn, setup_pt);
-    read_xml(setup_config_filename,   setup_pt,   boost::property_tree::xml_parser::trim_whitespace); // check where this flag actually lives
-    read_xml(session_config_filename, session_pt, boost::property_tree::xml_parser::trim_whitespace); // can/should put 2 possible fails in one try block?
+    const std::string ts= "/home/greghale/arte-ephys/conf/arte_setup_default.conf";
+    std::cout << setup_fn << std::endl;
+    read_xml(&ts, setup_pt);
+    read_xml(&setup_config_filename,   setup_pt,   boost::property_tree::xml_parser::trim_whitespace); // check where this flag actually lives
+    read_xml(&session_config_filename, session_pt, boost::property_tree::xml_parser::trim_whitespace); // can/should put 2 possible fails in one try block?
   }
   catch(...){  // find out where the xml_parse_error lives, & how to handle it
     std::cout << "XML read error was thrown - from arteopt.cpp" << std::endl;
