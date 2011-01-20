5361ffff9cd25ba143322e9b0813b932c11dfbb7
Stuart Layton <stuart.layton@gmail.com>
2011-01-20 16:53:18 +0000

    moved timer sources back into the main source folder

--- src/timer.h
+++ src/timer.h
@@ -1,48 +1,62 @@
-#include <iostream>
-#include <sys/time.h>
-
-
-//enum timer_role_t {TIMER_ROLE_MASTER = 0, TIMER_ROLE_SLAVE = 1}; // a timer_role type 
-//enum timer_method{TIMER_METHOD_SYSCLOCK = 0, TIMER_METHOD_COUNTERCARD = 1}; // the type of timer
-
+#include <nidaqmx.h>
+#include <stdint.h>
+#include <string>
 class Timer {
-  
+
  public:
-  
-  Timer();
+
+  Timer(string deviceID);
+
   virtual ~Timer();
-  int timer_init();
 
-  unsigned long get_count();
+  int startTimer();
+  int stopTimer();
 
-  void set_count(unsigned long); // an override function.  Generally shouldn't be used.
+  int setInitCount(uint16_t initCount);
+  uint32_t getCount();
+  uint64_t getTimestamp();
 
-  int write_count_to_es();
-  int write_count_to_es_with_message(char *msg);
+  int setTimerID(int8_t newId);
+  int8_t getTimerID();
 
-  int handle_sync_prepare_pulse();
-  int handle_sync_start_pulse();
-  int handle_tic_pulse();
-
-  // make a packet giving the count value at current sys time.  Send it to
-  // another machine to check if its count value was the same at its systime
-  unsigned long test_sysclock_sync(int); 
+  const int N_MASTER_SYNC_TX = 10;
+  const int N_SLAVE_SYNC_TX =1;
 
  private:
-  //timer_role_t timer_role; // is this master timer or slave?
-  
-  int timer_role;
-  unsigned long raw_count_value; // count value on the countercard, if we read that
-  unsigned long count_offset_value; // we may want to store an offset
-  bool timer_armed; // armed = able to start running at the next sync-start pulse?
-  bool timer_running; // running = incrementing at each tic pulse
-  unsigned long hotstart_count_value; // after power-cycling as lave, this is part of the re-sync protocol
-  timeval sys_time_at_counter_start;
 
-  double tics_per_sec;
-  double secs_per_tic;
+  //Methods
+  int txSyncCount(uint32_t syncCount, int nPacket);
+  uint32_t rxSyncCount();
+  int becomeMaster();
+  int armTimer();
 
-  unsigned long get_count_by_sysclock();
-  unsigned long get_count_by_countercard();
+  int initDAQCounterTask();
+  int initDAQSyncTask();
+  int armCounterTask();
+ 
+  //naming syntax required for the  NIDaqMx signalChange
+  int32 CVICALLBACK digitalChangeCallback(TaskHandel taskHandle, int32 signalID, void *callbackData);
 
-};
+ //Variables
+  string niDevice;
+  int8_t timerId;
+ 
+  bool running;
+  bool armed;
+  bool master; 
+  bool isSynced;
+
+  uint32_t initCount;
+  int countDir;
+  int countEdge;
+
+  int txSyncPort;
+  int rxSyncPort;
+
+  uint32_t prevSyncCount;
+  uint32_t curSyncCount;
+
+  TaskHandle counterTask;
+  TaskHandle diPulseTask;
+
+}
