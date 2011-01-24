52d726f6f500b4826f6a3364fcdd0568215f1eca
Stuart Layton <stuart.layton@gmail.com>
2011-01-24 20:33:04 +0000

    renamed tests

---
+++ src/bcast.cpp
@@ -0,0 +1,16 @@
+#include "netcom.h"
+
+int main(){
+	
+//	char host[] = "10.121.43.155";
+	char host[] = "10.121.43.255";
+//	char host[] = "localhost";
+	std::cout<<"Broadcasting to:"<<host<<std::endl;
+	for (int i=0; i<10; i++)
+	{
+		NetComDat net = NetCom::initUdpTx(host,4000);
+		NetCom::txSyncCount(net, i*1000, i);
+	}
+
+	return 0;
+}
