49dabbc18d89d96835714e7b9a32517f901c9127
Greg Hale <imalsogreg@gmail.com>
2011-01-25 01:16:54 +0000

    Oops, committed from the wrong directory before.  Adding test code for filt.

---
+++ test/test_filt.cpp
@@ -0,0 +1,47 @@
+#include "trode.h"
+#include "arteopt.h"
+#include "global_defs.h"
+#include "filt.h"
+#include <NIDAQmx.h>
+#include <map>
+#include <iostream>
+
+int main(int argc, char *argv[]){
+
+  arte_init(argc, argv, default_setup_config_filename, default_session_config_filename);
+
+  int n_chans = trode_map["tt00"].trode_opt.n_chans;
+  int buffer_len = trode_map["tt00"].my_daq->n_samps_per_buffer;
+  int n_buffers_to_make = 1;
+
+  float64 A = 2.0;
+  int T = 10; // square wave period in sample units
+  
+  float64 *this_buffer;
+  std::cout << "old ptr: " << trode_map["tt00"].my_daq->data_ptr << " ";
+  
+  // this_buffer = new float64 [(n_chans * buffer_len)];
+  
+  //trode_map["tt00"].my_daq->data_ptr = this_buffer;
+  //trode_map["tt00"].my_daq->data_ptr_copy = this_buffer;
+  //trode_map["tt00"].my_daq->copy_flexptr = this_buffer;
+  //trode_map["tt00"].pp_to_raw_stream = &this_buffer;
+  
+  std::cout << "local ptr: " << this_buffer << "  new daq ptr: " << trode_map["tt00"].my_daq->data_ptr << std::endl;
+
+  for(int s = 0; s < buffer_len; s++){
+    int h = s * n_chans;
+    for(int c = 0; c < n_chans; c++){
+      if( (s+T % T) < T/2) {
+	this_buffer[h + c] = -1*A;}
+      else{
+	this_buffer[h + c] = A;
+      }
+    }
+  }
+  //  memcpy( trode_map["tt00"].my_daq->data_ptr_copy, this_buffer, (n_chans * buffer_len * 8) );
+  trode_filter_data(&(trode_map["tt00"]));
+  trode_map["tt00"].print_buffers(4,97);
+
+  return 0;
+}
