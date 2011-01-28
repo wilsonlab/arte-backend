18ce9d93f3055980913e622a3fe967ea8e1afdd1
Stuart Layton <stuart.layton@gmail.com>
2011-01-28 03:48:18 +0000

    moved a few test files into the testing folder, where they belong

--- src/netcom/datapacket.h
+++ src/netcom/datapacket.h
@@ -1,11 +1,35 @@
-#ifndef NETCOM_SYNCPACKET_H
-#define NETCOM_SYNCPACKET_H
+#ifndef NETCOM_DATAPACKET_H
+#define NETCOM_DATAPACKET_H
 
 #include <stdint.h>
 #include <netcom.h>
 
+// Each buffer has a 2 byte header, with the first byte corresponding to 
+// the type of data contained in the buffer, the second byte is set to 0 
+// but this isn't currently required as maybe in the future we will see 
+// a need to add more information to the buffer header
+//
+// ??should we explicity test to see if the 2nd byte is set to zero as a 
+// sanity test on the buffer? Not sure we should discuss this
+//
+// Unless otherwise specified byts in each packet are arranged 
+// such that the first var in the type def corresponds to 
+// the first set of bytes in the buffer, the next var is found in the 
+// directly after the bytes allocated for the previous var and so on.
+// Each var uses exactly the number of bytes in the buffer that uses in 
+// the struct, for Example:
+//
+// if I had  struct data_t{
+// uint8_t v1;
+// uint8_t v2;
+// uint16_t v3;
+// };
+//
+// the data in the packed buffer would look like:
+// [BYTE0 |  BYTE1 | BYTE2 | BYTE3 | BYTE4 | BYTE5 ]
+// [DTYPE |    0   | VAR1  |  VAR2 |  VAR3 | VAR3  ]
+//
 typedef uint32_t timestamp_t;
-
 
 struct spike_t{
 	timestamp_t ts;
@@ -30,11 +54,11 @@ struct wave_t{
 void tsToBuff(timestamp_t* s,char* buff, int blen);
 timestamp_t buffToTs(char* buff, int blen);
 
-void spikeToBuff(spikepacket_t* s, char* buff, int blen);
-spikepacket_t buffToSpike(char *buff, int blen);
+void spikeToBuff(spike_t* s, char* buff, int blen);
+spike_t buffToSpike(char *buff, int blen);
 
-void waveToBuff(wavepacket_t* w, char* buff, int blen);
-wavepacket_t buffToWave(char* buff, int blen);
+void waveToBuff(wave_t* w, char* buff, int blen);
+wave_t buffToWave(char* buff, int blen);
 
 
 #endif
