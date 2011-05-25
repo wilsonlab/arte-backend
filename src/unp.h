0dcdf6d85d36313339892c0f3d1262080979d9e2
Greg Hale <imalsogreg@gmail.com>
2011-05-25 00:03:33 +0000

    Found problem - very innocent looking macro called min defined in unp.h seemed to conflit with some filesystem library thing.  Renamed min and max macros to MIN and MAX, and compile works.

--- src/unp.h
+++ src/unp.h
@@ -53,124 +53,136 @@
 // include <pthread.h>
 // endif
 
-#ifdef __osf__
-#undef recv
-#udef send
-#define recv(a,b,c,d)   recvfrom(a,b,c,d,0,0)
-#define send(a,b,c,d)   sendto(a,b,c,d,0,0)
-#endif
 
-#ifndef INADDR_NONE
-#define INADDR_NONE 0xffffffff // should have been in <netinet/in.h>
-#endif
 
-#ifndef SHUT_RD                // these three Posix.1g names are quite new
-#define SHUT_RD    0           // shutdown for reading
-#define SHUT_WR    1           // shutdown for writing
-#define SHUT_RDWR  2           // shutdown for reading and writing
-#endif
-#ifndef INET_ADDRSTRLEN
-#define INET_ADDRSTRLEN    16   // "ddd.ddd.ddd.ddd\0"
-#endif
+ #ifdef __osf__ 
+ #undef recv 
+ #udef send 
+ #define recv(a,b,c,d)   recvfrom(a,b,c,d,0,0) 
+ #define send(a,b,c,d)   sendto(a,b,c,d,0,0) 
+ #endif 
 
-// define following even if IPv6 not supported, so we can
-// always allocate an adequately-sized buffer, 
-// without #ifdefs in the code
-#ifndef INET6_ADDRSTRLEN
-#define INET6_ADDRSTRLEN    46
-#endif
+ #ifndef INADDR_NONE 
+ #define INADDR_NONE 0xffffffff // should have been in <netinet/in.h> 
+ #endif 
 
-// define bzero() as a macro if it's not in standard C library
-#ifndef HAVE_BZERO
-#define bzero(ptr,n)    memset(ptr,0,n)
-#endif
+ #ifndef SHUT_RD                // these three Posix.1g names are quite new 
+ #define SHUT_RD    0           // shutdown for reading 
+ #define SHUT_WR    1           // shutdown for writing 
+ #define SHUT_RDWR  2           // shutdown for reading and writing 
+ #endif 
+ #ifndef INET_ADDRSTRLEN 
+ #define INET_ADDRSTRLEN    16   // "ddd.ddd.ddd.ddd\0" 
+ #endif 
 
-// older resolvers don't have gethostbyname2()
-#ifndef HAVE_GETHOSTBYNAME2
-#define gethostbyname2(host,family)    gethostbyname((host))
-#endif
+ // define following even if IPv6 not supported, so we can 
+ // always allocate an adequately-sized buffer,  
+ // without #ifdefs in the code 
+ #ifndef INET6_ADDRSTRLEN 
+ #define INET6_ADDRSTRLEN    46 
+ #endif 
 
-// The structure returned by recvfrom_flags()
-//struct in_pktinfo{
-//  struct in_addr ipi_addr;    // dst IPv4 address
-//  int            ipi_ifindex; // received interface index
-//};
+ // define bzero() as a macro if it's not in standard C library 
+ #ifndef HAVE_BZERO 
+ #define bzero(ptr,n)    memset(ptr,0,n) 
+ #endif 
 
-// We need the newer CMSG_LEN() and CMSG_SPACE() macros, but few
-// implementations support them today.  These two macros really
-// need an ASIGN() macro, but each implementation does this differently
-#ifndef CMSG_LEN
-#define CMSG_LEN(size)   (sizeof(struct cmsghdr) + (size))
-#endif
-#ifndef CMSG_SPACE
-#define CMSG_SPACE(size) (sizeof(struct cmsghdr) + (size))
-#endif
+ // older resolvers don't have gethostbyname2() 
+ #ifndef HAVE_GETHOSTBYNAME2 
+ #define gethostbyname2(host,family)    gethostbyname((host)) 
+ #endif 
 
-// Posix.1g requires the SUN_LEN() macro but not all implementations define
-// it (yet). Note that this 4.4BSD macro works regardless whether there is
-// a length field or not
-#ifndef SUN_LEN
-#define SUN_LEN(su)  (sizeof(*(su)) - sizeof((su)->sun_path) + strlen((su)->sun_path))
-#endif
+ // The structure returned by recvfrom_flags() 
+ //struct in_pktinfo{ 
+ //  struct in_addr ipi_addr;    // dst IPv4 address 
+ //  int            ipi_ifindex; // received interface index 
+ //}; 
 
-// Posix.1g renames "Unix domain" as "local IPC"
-// But not all systems define AF_LOCAL and PF_LOCAL (yet)
-#ifndef AF_LOCAL
-#define AF_LOCAL    AF_UNIX
-#endif
-#ifndef PF_LOCAL
-#define PF_LOCAL    PF_UNIX
-#endif
+ // We need the newer CMSG_LEN() and CMSG_SPACE() macros, but few 
+ // implementations support them today.  These two macros really 
+ // need an ASIGN() macro, but each implementation does this differently 
+ #ifndef CMSG_LEN 
+ #define CMSG_LEN(size)   (sizeof(struct cmsghdr) + (size)) 
+ #endif 
+ #ifndef CMSG_SPACE 
+ #define CMSG_SPACE(size) (sizeof(struct cmsghdr) + (size)) 
+ #endif 
 
-// Posix.1g requires that an #include of <poll.h> define INFTIM, but many
-// systems still define it in <sys/stropts.h>  We don't want to include
-// all the streams stuff if it's not needed, so we just define INFTIM here.
-// This is the standard value, but there's no guarentee it is -1
-#ifndef INFTIM
-#define INFTIM    (-1)    // infinite poll timeout
-#ifdef HAVE_POLL_H
-#define INFTIM_UNPH       // tell unpxti.h we defined it
-#endif
-#endif
+ // Posix.1g requires the SUN_LEN() macro but not all implementations define 
+ // it (yet). Note that this 4.4BSD macro works regardless whether there is 
+ // a length field or not 
+ #ifndef SUN_LEN 
+ #define SUN_LEN(su)  (sizeof(*(su)) - sizeof((su)->sun_path) + strlen((su)->sun_path)) 
+ #endif 
 
-// Following could be derived from SOMAXCONN in  <sys/socket.h>, but many
-// kernels still #define it as 5, while actually supporting many more
-#define LISTENQ    1024
+ // Posix.1g renames "Unix domain" as "local IPC" 
+ // But not all systems define AF_LOCAL and PF_LOCAL (yet) 
+ #ifndef AF_LOCAL 
+ #define AF_LOCAL    AF_UNIX 
+ #endif 
+ #ifndef PF_LOCAL 
+ #define PF_LOCAL    PF_UNIX 
+ #endif 
 
-// Miscillaneous constants
-#define MAXLINE       4096   // max text line length
-#define MAXSOCKADDR    128   // max socket address structure size
-#define BUFFSIZE      8192   // buffer size for reads and writes
 
-// Define some port number that can be used for client-servers
-#define SERV_PORT      9877   // TCP and UDP client-servers
-#define SERV_PORT_STR "9877"  // TCP and UDP client-servers
-#define UNIXSTR_PATH  "/tmp/unix.str"  // Unix domain stream cli-serv
-#define UNIXDG_PATH   "/tmp/unix.dg"   // Unix domain datagram cli-serv
 
-// Following shortens all the type casts of pointer arguments
-#define SA struct sockaddr
 
-#define FILE_MODE    (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
-//                    default file access permissions for new files
-#define DIR_MODE     (FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH) 
+ // Posix.1g requires that an #include of <poll.h> define INFTIM, but many 
+ // systems still define it in <sys/stropts.h>  We don't want to include 
+ // all the streams stuff if it's not needed, so we just define INFTIM here. 
+ // This is the standard value, but there's no guarentee it is -1 
+ #ifndef INFTIM 
+ #define INFTIM    (-1)    // infinite poll timeout 
+ #ifdef HAVE_POLL_H 
+ #define INFTIM_UNPH       // tell unpxti.h we defined it 
+ #endif 
+ #endif 
 
-typedef void Sigfunc (int);   // for signal handlers
+ // Following could be derived from SOMAXCONN in  <sys/socket.h>, but many 
+ // kernels still #define it as 5, while actually supporting many more 
+ #define LISTENQ    1024 
 
-#define min(a,b)   ((a) < (b) ? (a) : (b))
-#define max(a,b)   ((a) > (b) ? (a) : (b))
+ // Miscillaneous constants 
+ #define MAXLINE       4096   // max text line length 
+ #define MAXSOCKADDR    128   // max socket address structure size 
+ #define BUFFSIZE      8192   // buffer size for reads and writes 
 
-// will this work?
-//#ifndef HAVE_ADDRINFO_STRUCT
-//#include "../lib/addrinfo.h"
-//#endif
 
-#ifndef HAVE_IF_NAMEINDEX_STRUCT
-struct if_nameindex {
-  unsigned int if_index;   // 1, 2, ...
-  char *if_name;           // sull terminated name: "1e0", ...
-};
-#endif
+
+
+ // Define some port number that can be used for client-servers 
+ #define SERV_PORT      9877   // TCP and UDP client-servers 
+ #define SERV_PORT_STR "9877"  // TCP and UDP client-servers 
+ #define UNIXSTR_PATH  "/tmp/unix.str"  // Unix domain stream cli-serv 
+ #define UNIXDG_PATH   "/tmp/unix.dg"   // Unix domain datagram cli-serv 
+
+ // Following shortens all the type casts of pointer arguments 
+ #define SA struct sockaddr 
+
+ #define FILE_MODE    (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH) 
+ //                    default file access permissions for new files 
+ #define DIR_MODE     (FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)  
+
+ typedef void Sigfunc (int);   // for signal handlers 
+
+// Commented b/c these guys break the compile
+ #define MIN(a,b)   ((a) < (b) ? (a) : (b)) 
+ #define MAX(a,b)   ((a) > (b) ? (a) : (b)) 
+
+/* // will this work? */
+/* //#ifndef HAVE_ADDRINFO_STRUCT */
+/* //#include "../lib/addrinfo.h" */
+/* //#endif */
+
+ #ifndef HAVE_IF_NAMEINDEX_STRUCT 
+ struct if_nameindex { 
+   unsigned int if_index;   // 1, 2, ... 
+   char *if_name;           // sull terminated name: "1e0", ... 
+ }; 
+ #endif 
+
+
+
 
 //#ifndef HAVE_TIMESPEC_STRUCT
 //struct timespec{
