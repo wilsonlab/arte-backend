a3f9ae0b3ce4ee3f58796759b2183a80dfaf0e48
Stuart Layton <stuart.layton@gmail.com>
2011-06-29 03:29:48 +0000

    updated spike viewer to plot (not working), added python script to plot via html5 canvas (super sexy)

--- src/python/html5ArteSpikeViewer.py
+++ src/python/html5ArteSpikeViewer.py
@@ -1,20 +1,38 @@
+import matplotlib
+matplotlib.use('module://mplh5canvas.backend_h5canvas')
+from pylab import *
+import time
+
 import sys
 import struct
-import matplotlib.pyplot as plot
-import pylab as p
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
 
+MAX_VOLTAGE = 1500;
 
 class RxArteUDP(DatagramProtocol):	
 
 	def __init__(self):
+		
+		self.packetCount = 0;
 		self.specifyBufferVars()	
 #		self.ax = p.subplot(111)
 #		self.canvas = self.ax.figure.canvas
-		self.line, = p.plot(xrange(0,128), xrange(0,128), animated=True);		
+#		self.line, = p.plot(xrange(0,128), xrange(0,128), animated=True);		
 #		self.bg = self.canvas.copy_from_bbox(self.ax.bbox)
-		p.draw()
+#		p.draw()
+
+		self.t = xrange(0,self.buffSize)
+		s = xrange(0,self.buffSize)
+		plot(self.t, s, linewidth=1.0)
+		ylabel('voltage (ad units)')
+		title('ARTE HTML5 Spike Viewer')
+		self.f = gcf()
+		self.ax = self.f.gca()
+		self.ax.set_xlim(0, 128)
+		self.ax.set_ylim(0, MAX_VOLTAGE)
+		show(block=False)
+
 	def specifyBufferVars(self, trimTopN = 16, nSamples = 32, nChannels= 4, bufferSize = 128):
 		self.trimTop = trimTopN
 		self.nSamp = nSamples
@@ -23,6 +41,7 @@ class RxArteUDP(DatagramProtocol):
 
 	def datagramReceived(self, data, (host, port)):
 		print "received from %s:%d " %(host, port)
+		self.packetCount = self.packetCount + 1
 		data = self.parseData(data)		
 		self.plotData(data)
 	
@@ -32,14 +51,15 @@ class RxArteUDP(DatagramProtocol):
 		for i in xrange(0,self.buffSize):
 			idx = i*2 + self.trimTop
 			wave[i] = (struct.unpack(">H", data[idx:idx+2]))[0]
+		wave[0] = MAX_VOLTAGE * (self.packetCount%30) / 30
+		wave[1] = wave[0]
 		return wave
 
 	def plotData(self, data):
-#		self.canvas.restore_region(self.bg)
-		self.line.set_ydata(data)
-#		self.ax.draw_artist(self.line)
-#		self.canvas.blit(ax.bbox)
-#		self.canvas.draw()
+		self.ax.lines[0].set_xdata(self.t)
+		self.ax.lines[0].set_ydata(data)
+		self.f.canvas.draw()
+		#self.line.set_ydata(data)
 		return 
 def main():
 	port = 5000;
