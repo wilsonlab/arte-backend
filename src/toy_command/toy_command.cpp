1676ca9d5bb67d02de00c2496b9476a1b3739f6c
Greg Hale <imalsogreg@gmail.com>
2011-07-15 02:30:46 +0000

    update from devel4

--- src/toy_command/toy_command.cpp
+++ src/toy_command/toy_command.cpp
@@ -22,14 +22,23 @@ int main (int argc, char *argv[]){
 
   while(running){
 
-    printf("\nPlease enter a command for arte:\n");
+    printf("\nPlease enter a command for arte, or type exit to quit:\n");
     fgets( the_command.command_str, MAX_COMMAND_STR_LEN+1, stdin );
     
-    printf("command: %s\n", the_command.command_str);
+    for(int i = 0; i < MAX_COMMAND_STR_LEN + 1; i++){
+      if(the_command.command_str[i] == '\n'){
+	//printf("Caught newline! at i=%d\n",i);
+	the_command.command_str[i] = '\0';
+	the_command.n_char = i;
+	break;
+      }
+    }
 
-    if(strcmp(the_command.command_str, "exit") == 0){
+    if( strcmp(the_command.command_str, "exit") == 0 ){
       running = false;
     } else{
+      //printf("about to send command:[%s]  strlen is:%d\n",
+	     the_command.command_str,strlen(the_command.command_str));
       NetCom::txCommand(my_netcom_dat, &the_command);
     } 
   }
