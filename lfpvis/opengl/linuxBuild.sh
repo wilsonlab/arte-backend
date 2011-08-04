bcc05c2e828a9566fd52d8a58f8c0d6e0b0419c1
Stuart Layton <stuart.layton@gmail.com>
2011-08-04 21:59:19 +0000

    updated build script to include .h file

--- lfpvis/opengl/linuxBuild.sh
+++ lfpvis/opengl/linuxBuild.sh
@@ -1,1 +1,1 @@
- g++ -lglut -lGL -lGLU -lGLEW  -I ../../src/netcom/  -D__STDC_LIMIT_MACROS simpleLfpPlot.cpp ../../src/netcom/*.cpp -o arteLfpViewer -g
+ g++ -lglut -lGL -lGLU -lGLEW -I ./ -I ../../src/netcom/  -D__STDC_LIMIT_MACROS simpleLfpPlot.cpp ../../src/netcom/*.cpp -o arteLfpViewer -g
