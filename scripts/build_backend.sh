79b92d0501995ce219b43b165f5001081aa5a6c5
Greg Hale <imalsogreg@gmail.com>
2011-08-09 02:40:32 +0000

    added build_backend.sh

---
+++ scripts/build_backend.sh
@@ -0,0 +1,6 @@
+clear
+rm test7
+rm test.txt
+rm *.filt
+rm *.unfilt
+g++ src/backend/arte.cpp src/backend/arteopt.cpp src/backend/filt.cpp src/backend/neural_daq.cpp src/backend/trode.cpp src/common/util.cpp src/backend/lfp_bank.cpp src/backend/filtered_buffer.cpp src/backend/filter_fun.cpp src/timer/timer.cpp src/backend/process_command.cpp src/netcom/netcom.cpp src/netcom/datapacket.cpp -pthread -ldl -lnidaqmx -I /usr/local/boost_1_45_0/ -I include/backend -I include/common -I include/timer -I include/netcom -D__STDC_LIMIT_MACROS -g -o build/arte_backend
