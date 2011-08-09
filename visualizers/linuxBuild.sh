b1801a721530ac5e27669ab4a8b8460946c2c564
Stuart Layton <stuart.layton@gmail.com>
2011-08-09 14:35:38 +0000

    more moving

---
+++ visualizers/linuxBuild.sh
@@ -0,0 +1,1 @@
+ g++ -lglut -lGL -lGLU -lGLEW  -I ../../src/netcom/  -D__STDC_LIMIT_MACROS simpleSpikePlot.cpp ../../src/netcom/*.cpp -o arteSpikeViewer
---
+++ visualizers/macBuild.sh
@@ -0,0 +1,1 @@
+ g++ -framework GLUT -framework OpenGL -framework Cocoa  -I ../../src/netcom/  -D__STDC_LIMIT_MACROS simpleSpikePlot.cpp ../../src/netcom/*.cpp -o arteSpikeViewer 
