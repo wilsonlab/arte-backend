fea8b7313c8f2f780af6d4b7b4bdc5f4647ce095
Josh Siegle <josh.siegle@gmail.com>
2011-09-05 18:39:05 +0000

    updated SplitterEditor

--- src/GUI/Source/Processors/Editors/SplitterEditor.cpp
+++ src/GUI/Source/Processors/Editors/SplitterEditor.cpp
@@ -8,3 +8,72 @@
   ==============================================================================
 */
 
+#include "SplitterEditor.h"
+
+PipelineSelectorButton::PipelineSelectorButton()
+	: DrawableButton (T("Selector"), DrawableButton::ImageFitted)
+{
+	DrawablePath normal, over, down;
+
+	    Path p;
+        p.addTriangle (0.0f, 0.0f, 0.0f, 20.0f, 18.0f, 10.0f);
+        normal.setPath (p);
+        normal.setFill (Colours::lightgrey);
+        normal.setStrokeThickness (0.0f);
+
+        over.setPath (p);
+        over.setFill (Colours::black);
+        over.setStrokeFill (Colours::black);
+        over.setStrokeThickness (5.0f);
+
+        setImages (&normal, &over, &over);
+        setBackgroundColours(Colours::darkgrey, Colours::purple);
+        setClickingTogglesState (true);
+        setTooltip ("Toggle a state.");
+
+}
+
+PipelineSelectorButton::~PipelineSelectorButton()
+{
+}
+
+
+SplitterEditor::SplitterEditor (GenericProcessor* parentNode, FilterViewport* vp) 
+	: GenericEditor(parentNode, vp)
+
+{
+	desiredWidth = 95;
+
+	pipelineSelectorA = new PipelineSelectorButton();
+	pipelineSelectorA->addListener(this);
+	pipelineSelectorA->setBounds(30,30,30,30);
+	pipelineSelectorA->setToggleState(true,false);
+	addAndMakeVisible(pipelineSelectorA);
+
+	pipelineSelectorB = new PipelineSelectorButton();
+	pipelineSelectorB->addListener(this);
+	pipelineSelectorB->setBounds(30,60,30,30);
+	pipelineSelectorB->setToggleState(false,false);
+	addAndMakeVisible(pipelineSelectorB);
+
+}
+
+SplitterEditor::~SplitterEditor()
+{
+	deleteAllChildren();
+}
+
+void SplitterEditor::buttonClicked(Button* button)
+{
+	if (button == pipelineSelectorA)
+	{
+		pipelineSelectorA->setToggleState(true,false);
+		pipelineSelectorB->setToggleState(false,false);
+
+	} else if (button == pipelineSelectorB) 
+	{
+		pipelineSelectorB->setToggleState(true,false);
+		pipelineSelectorA->setToggleState(false,false);
+		
+	}
+}
--- src/GUI/Source/Processors/Editors/SplitterEditor.h
+++ src/GUI/Source/Processors/Editors/SplitterEditor.h
@@ -12,7 +12,35 @@
 #define __SPLITTEREDITOR_H_33F644A8__
 
 
+#include "../../../JuceLibraryCode/JuceHeader.h"
+#include "GenericEditor.h"
 
+class FilterViewport;
+
+class PipelineSelectorButton : public DrawableButton
+{
+	public:
+		PipelineSelectorButton();
+		~PipelineSelectorButton();	
+};
+
+class SplitterEditor : public GenericEditor,
+					   public Button::Listener
+{
+public:
+	SplitterEditor (GenericProcessor* parentNode, FilterViewport* vp);
+	virtual ~SplitterEditor();
+
+	void buttonClicked (Button* button);
+
+private:	
+	
+	PipelineSelectorButton* pipelineSelectorA;
+	PipelineSelectorButton* pipelineSelectorB;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SplitterEditor);
+
+};
 
 
 #endif  // __SPLITTEREDITOR_H_33F644A8__
