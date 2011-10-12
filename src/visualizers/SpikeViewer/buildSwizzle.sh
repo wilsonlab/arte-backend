8d5f5f52713140bb970f8dffcd8dc68cb7d18bcf
Stuart Layton <stuart.layton@gmail.com>
2011-10-12 21:01:43 +0000

    fixed spelling error in vertexArrayTest name

--- src/visualizers/SpikeViewer/buildSwizzle.sh
+++ src/visualizers/SpikeViewer/buildSwizzle.sh
@@ -1,1 +1,1 @@
- g++ -framework GLUT -framework OpenGl -framework Cocoa -I /opt/local/include/GL/ -L /opt/local/lib -lGLEW  swizzletest.cpp -o swizzle 
+ g++ -framework GLUT -framework OpenGl -framework Cocoa -I /opt/local/include/GL/ -L /opt/local/lib -lGLEW  vertexArrayTest.cpp -o vertexArrayTest 
