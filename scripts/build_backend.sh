91d4912648200a68566035a8c1beb0fa3ab6a6c7
Greg Hale <imalsogreg@gmail.com>
2012-04-20 02:14:23 +0000

    Restructured directories.  All backend stuff is now in /src.  We should move all other sub-programs into sub-directories within /src - e.g. src/extract/.  Also modified backend build script.

--- scripts/build_backend.sh
+++ scripts/build_backend.sh
@@ -3,4 +3,4 @@ rm test7
 rm test.txt
 rm *.filt
 rm *.unfilt
-g++ src/backend/arte.cpp src/backend/arteopt.cpp src/backend/filt.cpp src/backend/neural_daq.cpp src/backend/trode.cpp src/common/util.cpp src/backend/lfp_bank.cpp src/backend/filtered_buffer.cpp src/backend/filter_fun.cpp src/timer/timer.cpp src/backend/process_command.cpp src/netcom/netcom.cpp src/netcom/datapacket.cpp -pthread -ldl -lnidaqmx -I /usr/local/boost_1_45_0/ -I include/backend -I include/common -I include/timer -I include/netcom -D__STDC_LIMIT_MACROS -O3 -o build/arte_backend
+g++ src/arte.cpp src/arteopt.cpp src/filt.cpp src/neural_daq.cpp src/trode.cpp src/util.cpp src/lfp_bank.cpp src/filtered_buffer.cpp src/filter_fun.cpp src/timer.cpp src/process_command.cpp src/netcom.cpp src/datapacket.cpp -pthread -ldl -lnidaqmx -I /usr/local/boost_1_45_0/ -I src/ -D__STDC_LIMIT_MACROS -O3 -o build/arte_backend
