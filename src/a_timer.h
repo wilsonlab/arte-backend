d37f7b7a07e112fee616ff7f73124a812f8a77ec
Greg Hale <imalsogreg@gmail.com>
2012-05-03 14:33:02 +0000

    tiny amount of work toward a_timer.h

--- src/a_timer.h
+++ src/a_timer.h
@@ -8,6 +8,10 @@
 // -NIDAQmxTimer  counter or timer, using M-series multifunction National Instruments card
 // -PCI05Timer    counter or timer, using PCI-05 coutner card
 
+#include "arte_command.pb.h"
+
+enum timer_state_t {TIMER_UNINITIALIZED, TIMER_STOPPED, TIMER_ARMED, TIMER_RUNNING};
+
 class aTimer {
 
  public:
@@ -20,14 +24,20 @@ class aTimer {
   set_timer_id(int newID);
   uint32_t get_count();
   uint32_t get_timestamp();
+  double get_timestamp_double();
   double get_arte_time_to_secs_coeff();
   void   set_arte_time_to_secs_coeff(double new_coeff);
   increment();
   reset_count();
   int tic();
   uint32_t toc();
+  int process_command(ArteCommand& the_command);
   
  private:
+  int id;
+  bool is_clock_source, is_counter;
+  timer_state_t timer_state;
+  uint32_t last_retrieved_count;
   uint32_t tic_time;
   double arte_time_to_secs_coeff;
 };
