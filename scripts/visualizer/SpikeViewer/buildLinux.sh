63390db8753c2fd7ae0e335a189b165352aec59d
hsw28 <holdbackaglacier@yahoo.com>
2015-04-28 14:29:22 -0400

    updated to work with ubuntu

--- scripts/visualizer/SpikeViewer/buildLinux.sh
+++ scripts/visualizer/SpikeViewer/buildLinux.sh
@@ -1,1 +1,1 @@
-g++ -lglut -lGL -lGLU -lGLEW  -I ../include/netcom/ -I ../../../include/common/  -D__STDC_LIMIT_MACROS SpikeViewer.cpp TetrodePlot.cpp glutViewer.cpp PlotUtils.cpp ../include/netcom/*.cpp -o SpikeViewer 
+g++ -I ../include/netcom/ -I ../../../include/common/  -D__STDC_LIMIT_MACROS SpikeViewer.cpp TetrodePlot.cpp glutViewer.cpp PlotUtils.cpp ../include/netcom/*.cpp -o SpikeViewer -lglut -lGL -lGLU -lGLEW -lpthread
