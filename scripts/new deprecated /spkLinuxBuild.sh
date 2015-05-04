eab65094c736d21f4a54c5a784d748c50c9fa2bf
hsw28 <holdbackaglacier@yahoo.com>
2015-05-04 16:21:48 -0400

    more cleaning

--- scripts/new deprecated /spkLinuxBuild.sh
+++
@@ -1,1 +0,0 @@
- g++ -I ./netcom/ -I ../include/visualizers/ -I ../include/common/  -D__STDC_LIMIT_MACROS ./netcom/*.cpp ./simpleSpikePlot.cpp -o arteSpikeViewer -I -g -lglut -lGL -lGLU -lGLEW -lpthread
--- scripts/new deprecated /spkMacBuild.sh
+++
@@ -1,1 +0,0 @@
- g++ -framework GLUT -framework OpenGL -framework Cocoa  -I ../../src/netcom/  -D__STDC_LIMIT_MACROS simpleSpikePlot.cpp ../../src/netcom/*.cpp -o arteSpikeViewer 
